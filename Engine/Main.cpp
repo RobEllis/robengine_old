9a90f608c51e724abba9f66953d6042021ffdc5f
Rob Ellis (i7436846) <i7436846@bournemouth.ac.uk>
2017-11-03 13:08:41 +0000

    Initial Commit

---
+++ Engine/Main.cpp
@@ -0,0 +1,12 @@
+// Game Engine Programming Assignment
+// By Rob Ellis
+// Bournemouth University
+
+#include <iostream>
+#include "Core.h"
+
+int main(int argc, char* args[])
+{
+	Core *m_testCore = new Core();
+	return m_testCore->Launch();
+}
